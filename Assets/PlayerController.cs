2fc34418bc5c9979bb8d6be3243cf6a6ddaa649c
Aidan Marco <aidanmarco@me.com>
2019-05-19 13:21:35 -0600

    Jumping Fix#

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -4,15 +4,18 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    CharacterController characterController;
 
     public float speed = 6.0f;
     public float turnspeed = 5.0f;
     public float jumpSpeed = 50.0f;
 
-    public Rigidbody myRigidbody;
+    public float disttoGround = 0.0f;
 
+    Rigidbody myRigidbody;
 
+    bool IsGrounded() {
+        return Physics.Raycast(transform.position, -Vector3.up, disttoGround + 0.1f);
+    }
 
     void Start()
     {
@@ -20,31 +23,33 @@ public class PlayerController : MonoBehaviour
     }
 
     void Update()
-    { 
-        //Jumping
-        if (Input.GetKeyDown(KeyCode.Space))
-        {
-            myRigidbody.AddForce(0,jumpSpeed*100,0);
-        }
-        //Moving Forwards
-        if (Input.GetKey(KeyCode.W))
-        {
-            gameObject.transform.Translate(0, 0, 1 * speed * Time.deltaTime);
-        }
-        //Backpedaling
-        if (Input.GetKey(KeyCode.S))
-        {
-            gameObject.transform.Translate(0, 0, -1 * speed * Time.deltaTime);
-        }
-        //Turn Right
-        if (Input.GetKey(KeyCode.D))
-        {
-            gameObject.transform.Rotate(0, turnspeed, 0);
-        }
-        //Turn Left
-        if (Input.GetKey(KeyCode.A))
-        {
-            gameObject.transform.Rotate(0, -turnspeed, 0);
+    {
+        if (IsGrounded()) {
+            //Jumping
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                myRigidbody.AddForce(0, jumpSpeed * 100, 0);
+            }
+            //Moving Forwards
+            if (Input.GetKey(KeyCode.W))
+            {
+                gameObject.transform.Translate(0, 0, 1 * speed * Time.deltaTime);
+            }
+            //Backpedaling
+            if (Input.GetKey(KeyCode.S))
+            {
+                gameObject.transform.Translate(0, 0, -1 * speed * Time.deltaTime);
+            }
+            //Turn Right
+            if (Input.GetKey(KeyCode.D))
+            {
+                gameObject.transform.Rotate(0, turnspeed, 0);
+            }
+            //Turn Left
+            if (Input.GetKey(KeyCode.A))
+            {
+                gameObject.transform.Rotate(0, -turnspeed, 0);
+            }
         }
     }
 }
