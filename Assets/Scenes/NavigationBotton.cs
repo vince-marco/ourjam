5b92976e968d534e5b1c8037a5a0d326ab896fcf
Aidan Marco <aidanmarco@me.com>
2019-05-18 23:05:30 -0600

    Scene Navigation

---
+++ Assets/Scenes/NavigationBotton.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class NavigationBotton : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    public void Loadscene(string sceneName)
+    {
+        Debug.Log("sceneBuildIndex to load: " + sceneName);
+        SceneManager.LoadScene(sceneName);
+    }
+}
