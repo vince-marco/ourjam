85a927c84b4c71a615a8fde78333d11fed7799a2
Aidan Marco <aidanmarco@me.com>
2019-05-19 12:52:42 -0600

    Player Movement

---
+++ Assets/PlayerController.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    CharacterController characterController;
+
+    public float speed = 6.0f;
+    public float turnspeed = 5.0f;
+    public float jumpSpeed = 50.0f;
+
+    public Rigidbody myRigidbody;
+
+
+
+    void Start()
+    {
+        myRigidbody = GetComponent<Rigidbody>();
+    }
+
+    void Update()
+    { 
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            myRigidbody.AddForce(0,jumpSpeed*100,0);
+        }
+        if (Input.GetKey(KeyCode.W))
+        {
+            gameObject.transform.Translate(0, 0, 1 * speed * Time.deltaTime);
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            gameObject.transform.Translate(0, 0, -1 * speed * Time.deltaTime);
+        }
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            gameObject.transform.Rotate(0, turnspeed, 0);
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            gameObject.transform.Rotate(0, -turnspeed, 0);
+        }
+
+    }
+}
